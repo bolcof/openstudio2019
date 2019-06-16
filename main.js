1e68f8fe737fee323e6a31b27c6bb9cfa5a18e91
bolcof <sanctum.crusade.ex@gmail.com>
2019-06-16 14:51:17 +0900

    autofill M2は作った

--- main.js
+++ main.js
@@ -28,106 +28,98 @@ $(function(){
 
 	//modal
 	$("#modals-wrapper").hide();
-	$(".artist-modal").hide();
 
-	$("#01_asao").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#01_asao', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#01_asao_modal").fadeIn(500);
 	});
 
-	$("#02_usami").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#02_usami', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#02_usami_modal").fadeIn(500);
 	});
 
-	$("#03_endo").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#03_endo', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#03_endo_modal").fadeIn(500);
 	});
 
-	$("#04_oikawa").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#04_oikawa', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#04_oikawa_modal").fadeIn(500);
 	});
 
-	$("#05_okada").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#05_okada', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#05_okada_modal").fadeIn(500);
 	});
 
-	$("#06_kanai").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#06_kanai', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#06_kanai_modal").fadeIn(500);
 	});
 
-	$("#07_kobayashi").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#07_kobayashi', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#07_kobayashi_modal").fadeIn(500);
 	});
 
-	$("#08_shikichi").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#08_shikichi', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#08_shikichi_modal").fadeIn(500);
 	});
 
-	$("#09_shimizu").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#09_shimizu', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#09_shimizu_modal").fadeIn(500);
 	});
 
-	$("#10_shimizu").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#10_shimizu', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#10_shimizu_modal").fadeIn(500);
 	});
 
-	$("#11_shin_usami").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#11_shin', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#11_shin_modal").fadeIn(500);
 	});
 
-	$("#12_maeda").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#12_maeda', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#12_maeda_modal").fadeIn(500);
 	});
 
-	$("#13_li").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#13_li', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
-		$("#12_li_modal").fadeIn(500);
+		$("#13_li_modal").fadeIn(500);
 	});
 
-	$("#14_yoshida").click(function(){
-  		$('body').css('overflow', 'hidden');
+	$(".grades").on('click', '#14_yoshida', function(){
+  		$('body').css('overflow', 'visible');
 		$("#modals-wrapper").fadeIn(500);
 		$("#14_yoshida_modal").fadeIn(500);
 	});
 
-	$("#modals-bg").click(function(){
-  		$('body').css('overflow', 'visible');
-		$(".artist-modal").fadeOut(500);
-		$("#modals-wrapper").fadeOut(500);
-	});
-
 	//iOSだと動かない疑惑
-	$(document).on('click', '.back-button', function(){
+	$("#modals-wrapper").on('click', '.back-button', function(){
   		$('body').css('overflow', 'visible');
 		$(".artist-modal").fadeOut(500);
 		$("#modals-wrapper").fadeOut(500);
 	});
-	$(".back-button").on("click", function(){
-		alert("aaa");
+	$("#modals-wrapper").on("click", function(){
   		$('body').css('overflow', 'visible');
 		$(".artist-modal").fadeOut(500);
 		$("#modals-wrapper").fadeOut(500);
--- modalMaker.js
+++ modalMaker.js
@@ -1,14 +1,96 @@
 $(function(){
- 	$("#statement").click(function(){
- 		var id = "01_asao";
- 		var lab = "高山明";
- 		var nameJP = "浅尾 鷹也";
- 		var nameEN = "Takaya Asao";
- 		var worksNum = 2;
- 		var filename = ["01.png", "sumbnail.png"];
- 		var fileTitle = ["作品01", "作品２"];
- 		var almaMater = "九州産業大学芸術学部デザイン学科卒"; 
- 		var link = "https://vimeo.com/user15242827";
+ 	var info_M2 = [
+ 				["01_asao",		"高山明",	"浅尾 鷹也",		"Takaya Asao",		"九州産業大学芸術学部デザイン学科卒",			"https://twitter.com/sotky_a"],
+ 				["02_usami",	"桂英史",	"宇佐美 奈緒",	"Nao Usami",		"東京藝術大学 美術学部 デザイン科卒",			"http://naousami.com"],
+ 				["03_endo",		"佐藤雅彦",	"遠藤 紘也",		"Hiroya ENdo",		"東京造形大学 造形学部 デザイン学科 映画専攻卒",	"https://natsumioikawa.tumblr.com/"],
+ 				["04_oikawa",	"高山明",	"及川 菜摘",		"Natsumi Oikawa",	"多摩美術大学 美術学部 統合デザイン学科卒",		"http://hiroya-endo.net/"],
+ 				["05_okada",	"桂英史",	"岡田 直己",		"Naoki Okada",		"東京藝術大学 美術学部 デザイン科卒",			"http://bat-headache-ouchouchouch.tumblr.com/"],
+ 				["06_kanai",	"佐藤雅彦",	"金井 啓太",		"Keita Kanai",		"筑波大学 情報学群卒",							"https://volkhov.tumblr.com/"],
+ 				["07_kobayashi","桐山孝司",	"小林 颯",		"Hayate Kobayashi",	"慶應義塾大学 環境情報学部 環境情報学科卒",		"https://vimeo.com/user15242827"],	
+ 				["08_shikichi",	"高山明",	"敷地 理",		"Osamu Shikichi",	"武蔵野美術大学 造形学部 彫刻学科卒",			"https://mumumubrothers.wixsite.com/osamu-shikichi"],
+ 				["09_shimizu",	"桐山孝司",	"清水 愛恵",		"Manae Shimizu",	"首都大学東京 システムデザイン学部 インダストリアルアートコース卒",	"http://manaepf-tokyo.com"],
+ 				["10_shimizu",	"桐山孝司",	"清水 潤",		"Mitsuru Shimizu",	"東京藝術大学 美術学部 先端芸術表現科卒",			""],
+ 				["11_shin",		"畠山直哉",	"申 晛湜",		"Hyunsik Shin",		"早稲田大学 文化構想学部 表象・メディア論系卒",	""],
+ 				["12_maeda",	"桂英史",	"前田 博雅",		"Hiromasa Maeda",	"武蔵野美術大学 造形学部 映像学科卒",			""],
+ 				["13_li",		"高山明",	"李 和晋",		"Lee Hwajin",		"東京工芸大学芸術学部写真学科卒",				""],
+ 				["14_yoshida",	"畠山直哉",	"吉田 真也",		"Shiniya Yoshida",	"秋田公立美術大学 美術学部 ビジュアルアーツ専攻卒",""]
+ 			];
+
+ 	var info_M1 = [
+ 				["01_asao",		"高山明",	"浅尾 鷹也",		"Takaya Asao",		"九州産業大学芸術学部デザイン学科卒",			"https://twitter.com/sotky_a"],
+ 				["02_usami",	"桂英史",	"宇佐美 奈緒",	"Nao Usami",		"東京藝術大学 美術学部 デザイン科卒",			"http://naousami.com"],
+ 				["03_endo",		"佐藤雅彦",	"遠藤 紘也",		"Hiroya ENdo",		"東京造形大学 造形学部 デザイン学科 映画専攻卒",	"https://natsumioikawa.tumblr.com/"],
+ 				["04_oikawa",	"高山明",	"及川 菜摘",		"Natsumi Oikawa",	"多摩美術大学 美術学部 統合デザイン学科卒",		"http://hiroya-endo.net/"],
+ 				["05_okada",	"桂英史",	"岡田 直己",		"Naoki Okada",		"東京藝術大学 美術学部 デザイン科卒",			"http://bat-headache-ouchouchouch.tumblr.com/"],
+ 				["06_kanai",	"佐藤雅彦",	"金井 啓太",		"Keita Kanai",		"筑波大学 情報学群卒",							"https://volkhov.tumblr.com/"],
+ 				["07_kobayashi","桐山孝司",	"小林 颯",		"Hayate Kobayashi",	"慶應義塾大学 環境情報学部 環境情報学科卒",		"https://vimeo.com/user15242827"],	
+ 				["08_shikichi",	"高山明",	"敷地 理",		"Osamu Shikichi",	"武蔵野美術大学 造形学部 彫刻学科卒",			"https://mumumubrothers.wixsite.com/osamu-shikichi"],
+ 				["09_shimizu",	"桐山孝司",	"清水 愛恵",		"Manae Shimizu",	"首都大学東京 システムデザイン学部 インダストリアルアートコース卒",	"http://manaepf-tokyo.com"],
+ 				["10_shimizu",	"桐山孝司",	"清水 潤",		"Mitsuru Shimizu",	"東京藝術大学 美術学部 先端芸術表現科卒",			""],
+ 				["11_shin",		"畠山直哉",	"申 晛湜",		"Hyunsik Shin",		"早稲田大学 文化構想学部 表象・メディア論系卒",	""],
+ 				["12_maeda",	"桂英史",	"前田 博雅",		"Hiromasa Maeda",	"武蔵野美術大学 造形学部 映像学科卒",			""],
+ 				["13_li",		"高山明",	"李 和晋",		"Lee Hwajin",		"東京工芸大学芸術学部写真学科卒",				""],
+ 				["14_yoshida",	"畠山直哉",	"吉田 真也",		"Shiniya Yoshida",	"秋田公立美術大学 美術学部 ビジュアルアーツ専攻卒",""]
+ 			];
+
+ 	var works_M2 = [
+ 				[1,	"00001",	"00002",	"00003"],/*浅尾*/
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[1,	"00001",	"00002",	"00003"],/*及川*/
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[1,	"00001",	"00002",	"00003"],/*金井*/
+ 				[2,	"00001",	"00002",	"00003"],
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[1,	"00001",	"00002",	"00003"],
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[2,	"00001",	"00002",	"00003"],
+ 				[2,	"00001",	"00002",	"00003"],
+ 	];
+
+
+ 	var works_M１ = [
+ 				[1,	"00001",	"00002",	"00003"],/*浅尾*/
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[1,	"00001",	"00002",	"00003"],/*及川*/
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[1,	"00001",	"00002",	"00003"],/*金井*/
+ 				[2,	"00001",	"00002",	"00003"],
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[1,	"00001",	"00002",	"00003"],
+ 				[3,	"00001",	"00002",	"00003"],
+ 				[2,	"00001",	"00002",	"00003"],
+ 				[2,	"00001",	"00002",	"00003"],
+ 	];
+
+
+
+ 	for(var itr=0; itr<14; itr++){
+ 		var id = info_M2[itr][0];
+ 		var lab = info_M2[itr][1];
+ 		var nameJP = info_M2[itr][2];
+		var nameEN = info_M2[itr][3];
+ 		var worksNum = works_M2[itr][0];
+ 		var filename = ["01.jpg", "02.jpg", "03.jpg"];
+ 		var fileTitle = [works_M2[itr][1], works_M2[itr][2], works_M2[itr][3]];
+ 		var almaMater = info_M2[itr][4]; 
+ 		var link = info_M2[itr][5];
+
+ 		$(".m2").append(
+ 			'<div class="artist-wrapper">\n<div class="sumbnail" >\n<img src="artistInfomation/M2/'
+			//+ id + '/01.jpg">\n</div>\n<div class="infoText">\n<p class="lab">'
+			+ '09_shimizu' + '/01.jpg">\n</div>\n<div class="infoText">\n<p class="lab">'
+			+ lab + '研究室</p>\n<div class="name">\n<p class="nameJP">'
+			+ nameJP + '</p>\n<p class="nameEN">'
+			+ nameEN +'</p>\n</div>\n<div class="modalButton" id="'
+			+ id + '">More infomation</div>\n</div>\n</div>'
+		);
+
 
 		$("#modals-wrapper").append(
 			'<div class="artist-modal" id="' 
@@ -26,5 +108,8 @@ $(function(){
 		$("#" + id + "_modal").append('<div class="footer-column">\n<p class="alma-mater">'
 			+ almaMater + '</p>\n<div class="link">\n<a href="'
 			+ link + '">' + link +'</a>\n</div>\n</div>');
-	});
+	}
+
+	$(".artist-modal").hide();
+
 });
